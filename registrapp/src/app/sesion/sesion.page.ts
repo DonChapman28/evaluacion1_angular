9a3b97b6aa788f3115575dea0a4cdac45c0499a8
eSponjosa <e.rojascisterna@outlook.com>
2023-09-11 21:00:34 -0300

    v0.2
    ahora tiene validacion de espacios vacios

--- registrapp/src/app/sesion/sesion.page.ts
+++ registrapp/src/app/sesion/sesion.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sesion',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sesion.page.scss'],
 })
 export class SesionPage implements OnInit {
+nombre = '';
+clave = '';  
+dato: boolean = false;
 
-  constructor() { }
+
+
+  constructor(private alertContorller:AlertController,
+    private toastController: ToastController,private router: Router) { 
+
+    }
 
   ngOnInit() {
   }
+
+  async validar()
+  {
+    
+    const numeroLetrasNombre = this.nombre.length;
+    const numeroLetrasClave = this.clave.length;
+    console.log(numeroLetrasNombre)
+    console.log(numeroLetrasClave)
+
+    if (numeroLetrasClave === 0) {
+      const toast = await this.toastController.create({
+        message: 'El campo de contraseña no debe estar vacío',
+        buttons: ['OK'],
+        color: 'danger',
+        position : 'bottom',
+      });
+      await toast.present();
+    }
+    
+    if (numeroLetrasNombre === 0) {
+      const toast = await this.toastController.create({
+        message: 'El campo de nombre de usuario no debe estar vacío',
+        buttons: ['OK'],
+        color: 'danger',
+        position : 'bottom',
+      });
+      await toast.present();
+    }
+///-----------------------------------------------------------------
+    if (numeroLetrasNombre > 4 && numeroLetrasClave > 12) {
+      this.dato = true;
+    }
+
+    }  
 
 }
